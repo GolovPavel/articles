58acb9202f33952b598e7b5607904d9fbc56caf2
golovpavel <golovpasha@gmail.com>
2019-04-22 17:08:51 +0300

    First version of JVM article without images

--- JVMIntroductionArticle/code/Calculator.java
+++ JVMIntroductionArticle/code/Calculator.java
@@ -3,7 +3,15 @@ class Calculator {
 	public static void main(String[] args){
 		int a = Integer.valueOf(args[0]);
 		int b = Integer.valueOf(args[1]);
-		
-		System.out.println(a + b);
+
+		int result = 0;
+		for (int i = 0; i < 10000; i++){
+			result = makeCalculation(a, b);
+		}
+		System.out.println(result);
+	}
+
+	private static int makeCalculation(int a, int b) {
+		return a + b;
 	}
 }
